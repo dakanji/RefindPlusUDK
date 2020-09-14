d63bea576f37eba070ca91436df6c0d3cac842d3
dakanji <dakanji@users.noreply.github.com>
2020-09-14 07:45:55 +0300

    Update Build Script

--- 000-BuildScript/refindBuilder.sh
+++ 000-BuildScript/refindBuilder.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# ------------------------- #
- # USER EDIT SECTION START #
-
-GITHUB_USERNAME="YOUR_GITHUB_USERNAME"
-
-  # USER EDIT SECTION END #
-# ------------------------- #
-
-
 # Provide custom colors
 msg_info() {
     echo -e "\033[0;33m$1\033[0m"
@@ -41,35 +32,28 @@ msg_info '--------------------------------'
 sleep 1
 EDIT_BRANCH="${1:-GOPFix}"
 BASE_DIR="${HOME}/Documents/RefindPlus"
-BUILD_DIR="${BASE_DIR}/edk2"
-if [ ! -d "${BUILD_DIR}" ] ; then
-    msg_error "ERROR: Could not locate ${BUILD_DIR}"
+WORK_DIR="${BASE_DIR}/Working"
+EDK2_DIR="${BASE_DIR}/edk2"
+if [ ! -d "${EDK2_DIR}" ] ; then
+    msg_error "ERROR: Could not locate ${EDK2_DIR}"
     echo ''
     exit 1
 fi
-XCODE_DIR_REL="${BUILD_DIR}/Build/Refind/RELEASE_XCODE5"
-XCODE_DIR_DBG="${BUILD_DIR}/Build/Refind/DEBUG_XCODE5"
-XCODE_DIR_TMP="${BUILD_DIR}/Build-DBG/Refind/RELEASE_XCODE5"
+XCODE_DIR_REL="${EDK2_DIR}/Build/Refind/RELEASE_XCODE5"
+XCODE_DIR_DBG="${EDK2_DIR}/Build/Refind/DEBUG_XCODE5"
+XCODE_DIR_TMP="${EDK2_DIR}/Build-DBG/Refind/RELEASE_XCODE5"
 BINARY_DIR="${XCODE_DIR_REL}/X64"
-OUTPUT_DIR="${BUILD_DIR}/000-BOOTx64-Files"
-GLOBAL_FILE="${BUILD_DIR}/RefindPkg/refind/globalExtra.h"
-GLOBAL_FILE_TMP_REL="${BUILD_DIR}/RefindPkg/refind/globalExtra-REL.txt"
-GLOBAL_FILE_TMP_DBG="${BUILD_DIR}/RefindPkg/refind/globalExtra-DBG.txt"
-pushd ${BUILD_DIR} > /dev/null || exit 1
-git checkout rudk
-if [ -d "${BUILD_DIR}/RefindPkg-OLD" ] ; then
-    rm -fr "${BUILD_DIR}/RefindPkg-OLD"
-fi
-if [ -d "${BUILD_DIR}/RefindPkg" ] ; then
-    mv "${BUILD_DIR}/RefindPkg" "${BUILD_DIR}/RefindPkg-OLD"
-fi
-git clone "https://github.com/${GITHUB_USERNAME}/RefindPlus.git" RefindPkg
-pushd "${BUILD_DIR}/RefindPkg" > /dev/null || exit 1
-# See: https://stackoverflow.com/a/10312587/891636
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
-git pull --all
+OUTPUT_DIR="${EDK2_DIR}/000-BOOTx64-Files"
+GLOBAL_FILE="${EDK2_DIR}/RefindPkg/refind/globalExtra.h"
+GLOBAL_FILE_TMP_REL="${EDK2_DIR}/RefindPkg/refind/globalExtra-REL.txt"
+GLOBAL_FILE_TMP_DBG="${EDK2_DIR}/RefindPkg/refind/globalExtra-DBG.txt"
+
+pushd ${WORK_DIR} > /dev/null || exit 1
 git checkout ${EDIT_BRANCH}
+rm -fr "${EDK2_DIR}/RefindPkg"
+cp -fa "${WORK_DIR}" "${EDK2_DIR}/RefindPkg"
+rm -fr "${EDK2_DIR}/RefindPkg/.gitignore"
+rm -fr "${EDK2_DIR}/RefindPkg/.git"
 popd > /dev/null || exit 1
 
 
@@ -78,11 +62,11 @@ clear
 msg_info '## RefindPlusBuilder - Initial Clean Up ##'
 msg_info '------------------------------------------'
 sleep 1
-if [ -d "${BUILD_DIR}/Build-OLD" ] ; then
-    rm -fr "${BUILD_DIR}/Build-OLD"
+if [ -d "${EDK2_DIR}/Build-OLD" ] ; then
+    rm -fr "${EDK2_DIR}/Build-OLD"
 fi
-if [ -d "${BUILD_DIR}/Build" ] ; then
-    mv "${BUILD_DIR}/Build" "${BUILD_DIR}/Build-OLD"
+if [ -d "${EDK2_DIR}/Build" ] ; then
+    mv "${EDK2_DIR}/Build" "${EDK2_DIR}/Build-OLD"
 fi
 if [ -d "${OUTPUT_DIR}" ] ; then
     rm -fr "${OUTPUT_DIR}"
@@ -95,8 +79,9 @@ clear
 msg_info '## RefindPlusBuilder - Building REL Version ##'
 msg_info '----------------------------------------------'
 sleep 1
-if [ -d "${BUILD_DIR}/Build-TMP" ] ; then
-    rm -fr "${BUILD_DIR}/Build-TMP"
+pushd ${EDK2_DIR} > /dev/null || exit 1
+if [ -d "${EDK2_DIR}/Build-TMP" ] ; then
+    rm -fr "${EDK2_DIR}/Build-TMP"
 fi
 if [ -f "${GLOBAL_FILE}" ] ; then
     rm -fr "${GLOBAL_FILE}"
@@ -104,10 +89,11 @@ fi
 cp "${GLOBAL_FILE_TMP_REL}" "${GLOBAL_FILE}"
 source edksetup.sh BaseTools
 build
-if [ -d "${BUILD_DIR}/Build" ] ; then
+if [ -d "${EDK2_DIR}/Build" ] ; then
     cp "${BINARY_DIR}/refind.efi" "${OUTPUT_DIR}/BOOTx64-REL.efi"
-    mv "${BUILD_DIR}/Build" "${BUILD_DIR}/Build-TMP"
+    mv "${EDK2_DIR}/Build" "${EDK2_DIR}/Build-TMP"
 fi
+popd > /dev/null || exit 1
 echo ''
 msg_info 'Completed REL Build ...Preparing DBG Build'
 echo ''
@@ -119,8 +105,9 @@ clear
 msg_info '## RefindPlusBuilder - Building DBG Version ##'
 msg_info '----------------------------------------------'
 sleep 1
-if [ -d "${BUILD_DIR}/Build-DBG" ] ; then
-    rm -fr "${BUILD_DIR}/Build-DBG"
+pushd ${EDK2_DIR} > /dev/null || exit 1
+if [ -d "${EDK2_DIR}/Build-DBG" ] ; then
+    rm -fr "${EDK2_DIR}/Build-DBG"
 fi
 if [ -f "${GLOBAL_FILE}" ] ; then
     rm -fr "${GLOBAL_FILE}"
@@ -128,26 +115,26 @@ fi
 cp "${GLOBAL_FILE_TMP_DBG}" "${GLOBAL_FILE}"
 source edksetup.sh BaseTools
 build
-if [ -d "${BUILD_DIR}/Build" ] ; then
+if [ -d "${EDK2_DIR}/Build" ] ; then
     cp "${BINARY_DIR}/refind.efi" "${OUTPUT_DIR}/BOOTx64-DBG.efi"
-    mv "${BUILD_DIR}/Build" "${BUILD_DIR}/Build-DBG"
-    mv "${BUILD_DIR}/Build-TMP" "${BUILD_DIR}/Build"
+    mv "${EDK2_DIR}/Build" "${EDK2_DIR}/Build-DBG"
+    mv "${EDK2_DIR}/Build-TMP" "${EDK2_DIR}/Build"
     mv "${XCODE_DIR_TMP}" "${XCODE_DIR_DBG}"
 fi
-if [ -d "${BUILD_DIR}/Build-DBG" ] ; then
-    rm -fr "${BUILD_DIR}/Build-DBG"
+if [ -d "${EDK2_DIR}/Build-DBG" ] ; then
+    rm -fr "${EDK2_DIR}/Build-DBG"
 fi
+popd > /dev/null || exit 1
 echo ''
 msg_info 'Completed DBG Build'
 echo ''
 
 
-# Tidy up and return to original location
+# Tidy up
 if [ -f "${GLOBAL_FILE}" ] ; then
     rm -fr "${GLOBAL_FILE}"
 fi
 cp "${GLOBAL_FILE_TMP_REL}" "${GLOBAL_FILE}"
-popd > /dev/null || exit 1
 echo ''
 msg_status "RefindPlus EFI Files (BOOTx64): '${OUTPUT_DIR}'"
 msg_status "RefindPlus EFI Files (Others - DBG): '${XCODE_DIR_DBG}/X64'"
