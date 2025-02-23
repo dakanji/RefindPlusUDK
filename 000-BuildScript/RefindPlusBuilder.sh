24a72654d4ab25d6678c442e2691f9e64b629f8b
startergo <7897244+startergo@users.noreply.github.com>
2025-02-23 10:08:18 -0500

    Update RefindPlusBuilder.sh
    Check if the BasetoolsSHA.txt file exists before attempting to source it and creates it if it does not exist.

--- 000-BuildScript/RefindPlusBuilder.sh
+++ 000-BuildScript/RefindPlusBuilder.sh
@@ -149,7 +149,11 @@ if [ -f "${DUP_SHASUM}" ] ; then
     mv -f "${DUP_SHASUM}" "${TMP_SHASUM}"
     SHASUM_FIX='true'
 fi
-BASETOOLS_SHA_FILE="${EDK2_DIR}/000-BuildScript/RepoUpdateSHA.txt"
+BASETOOLS_SHA_FILE="${EDK2_DIR}/000-BuildScript/BaseToolsSHA.txt"
+if [ ! -f "${BASETOOLS_SHA_FILE}" ] ; then
+    echo '#!/usr/bin/env bash' > "${BASETOOLS_SHA_FILE}"
+    echo "BASETOOLS_SHA_OLD='Default'" >> "${BASETOOLS_SHA_FILE}"
+fi
 # shellcheck disable=SC1090
 source "${BASETOOLS_SHA_FILE}" || BASETOOLS_SHA_OLD='Default'
 Get_Sha_Str="$(find . -type f \( -name '*.c' -or -name '*.cpp' -or -name '*.h' -or -name '*.py' -or -name '*.makefile' -or -name 'GNUmakefile' \) -print0 | sort -z | xargs -0 ${OUR_SHASUM} | ${OUR_SHASUM} | cut -d ' ' -f 1)"
